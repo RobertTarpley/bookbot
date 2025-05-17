0f49f68ced4f8df713ab8ed5bddd2b9fa0015e39
RobertTarpley <131493065+RobertTarpley@users.noreply.github.com>
2025-05-17 16:39:57 -0400

    Added books, text and updated main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+def count_words(book_text):
+    words = book_text.split()
+    num_words = len(words)
+    return f"{num_words} words found in the document"
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    result = count_words(book_text)
+    print(result)
+
+main()

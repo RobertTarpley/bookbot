ea626485ee229d45bfd7c156dc2d1f0d5ddc07a5
RobertTarpley <131493065+RobertTarpley@users.noreply.github.com>
2025-05-18 11:54:36 -0400

    added count characters function and returned dictionary to main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,6 +7,7 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     result = count_words(book_text)
+    result2 = count_characters(book_text)
     print(result)
-
+    print(result2)
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,18 @@ def count_words(book_text):
     words = book_text.split()
     num_words = len(words)
     return f"{num_words} words found in the document"
+
+def count_characters(book_text):
+    character_dict = {}
+    for char in book_text.lower():  # Convert once before the loop
+        if char not in character_dict:
+            character_dict[char] = 1
+        else:
+            character_dict[char] += 1
+    return character_dict
+
+
+
+
+
+

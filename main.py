365041960275c55dc1c424e3b97e03759c1ed8ec
RobertTarpley <131493065+RobertTarpley@users.noreply.github.com>
2025-05-18 11:25:23 -0400

    added stats.py and moved count_words function to it

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+from stats import count_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
-
-def count_words(book_text):
-    words = book_text.split()
-    num_words = len(words)
-    return f"{num_words} words found in the document"
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(book_text):
+    words = book_text.split()
+    num_words = len(words)
+    return f"{num_words} words found in the document"
